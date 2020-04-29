be51f9a209e2a888ac828f52d4953a4ddc8c044d
fujiken7777 <v.i.p.ordermade@gmail.com>
2020-04-29 15:11:50 +0900

    commit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,22 +1,24 @@
-# 下記の三行は入れていた方がいいらしい
 require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-if Rails.env.production?
-  CarrierWave.configure do |config|
+CarrierWave.configure do |config|
+  if Rails.env.production? || Rails.env.development? # 開発中もs3使う
+    config.storage :fog
     config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'quizerimageupload'
+    config.asset_host = 'https://s3.amazonaws.com/quizerimageupload'
+    # NOTE: AWS側の設定を変えなくても、この１行の設定でアップロードできた
+    config.fog_public = false # ←コレ
     config.fog_credentials = {
-      # Amazon S3用の設定
       provider: 'AWS',
-      region: ENV['AWS_S3_REGION'],
       aws_access_key_id: ENV['AWS_IAM_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_IAM_ACCESS_KEY'],
+      region: ENV['AWS_S3_REGION'],
+      # path_style: true
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
-    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
   end
-
-  # 日本語ファイル名の設定
-  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 end
